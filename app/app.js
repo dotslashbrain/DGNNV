0cc384de6bb0ecea05e30dac5174149450f5d64a
dotslashbrain <derekpalmer0341@gmail.com>
2022-09-07 08:31:09 -0700

    Added the basic scaffolding for the C/C++ Neural Network API Calls

--- app/app.js
+++ app/app.js
@@ -24,18 +24,45 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
+
+
+// Require addons
+const calculate = require('./build/Release/calculate');
+
+// Javascript add function.
+function calc() {
+
+    // Two variables.
+    let i, x = 3.141526,
+        y = 2.718;
+    for (i = 0; i < 1000000000; i++) {
+        x += y;
+
+    }
+    let total = x;
+    return total;
+}
+
+console.time('c++');
+calculate.calc();
+console.timeEnd('c++');
+
+console.time('js');
+calc();
+
+console.timeEnd('js');
